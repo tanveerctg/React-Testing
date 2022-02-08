f08cf48832a31483d9aa735aa6cf29933bdb52e1
tanveer <tanveerctg2014@gmail.com>
2022-02-08 12:44:15 +0600

    tested todo component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [allTodos, setAllTodos] = useState([]);
+  const [todoName, setTododName] = useState("");
+  const [error, setError] = useState("");
+
+  const handleTodoChange = (e) => {
+    setTododName(e.target.value);
+  };
+  const handleAddTodo = () => {
+    //check todoName is empty or not
+    if (!todoName) {
+      setError("Todo field is empty.Please fill that field");
+      return;
+    }
+
+    const id = allTodos.length > 0 ? allTodos.length - 1 : 1;
+    setAllTodos((prev) => [...prev, { id, todoName }]);
+    setTododName("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      {allTodos.map((todo) => (
+        <div
+          key={todo.id}
+          style={{ display: "flex", alignItems: "center", gap: "40px" }}
+          data-testid="todo"
         >
-          Learn React
-        </a>
-      </header>
+          <h2>{todo.todoName}</h2>
+          <button
+            onClick={() =>
+              setAllTodos((prev) => prev.filter((t) => t.id !== todo.id))
+            }
+            data-testid={todo.id}
+          >
+            Delete todo
+          </button>
+        </div>
+      ))}
+      <input
+        onChange={handleTodoChange}
+        data-testid="todoInput"
+        // value={todoName}
+      />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      {error && <h4 id="errorMsg">{error}</h4>}
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,62 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("When App first render", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const inputElement = screen.getByRole("textbox");
+  const errorMsg = screen.queryByText(
+    "Todo field is empty.Please fill that field"
+  );
+  const allTodos = screen.queryAllByTestId("todo");
+
+  expect(inputElement).toHaveValue("");
+  expect(errorMsg).not.toBeInTheDocument();
+  expect(allTodos).toHaveLength(0);
+});
+
+test("When user add new todo", async () => {
+  render(<App />);
+  const inputElement = screen.getByRole("textbox");
+  const addTodoBtn = screen.getByRole("button");
+
+  //Before adding new todo todos length will be 0
+  expect(screen.queryAllByTestId("todo")).toHaveLength(0);
+  userEvent.type(inputElement, "Get Up Early");
+  userEvent.click(addTodoBtn);
+
+  //After adding new todo todos length will be 1
+  expect(await screen.findAllByTestId("todo")).toHaveLength(1);
+});
+
+test("Delete Todo", async () => {
+  render(<App />);
+  const inputElement = screen.getByRole("textbox");
+  const addTodoBtn = screen.getByRole("button");
+
+  //Before adding new todo todos length will be 0
+  expect(screen.queryAllByTestId("todo")).toHaveLength(0);
+  userEvent.type(inputElement, "Get Up Early");
+  userEvent.click(addTodoBtn);
+  //After adding new todo todos length will be 1
+  expect(await screen.findAllByTestId("todo")).toHaveLength(1);
+
+  //find that newly added todo by id
+  const findTodo = screen.queryByTestId(1);
+  userEvent.click(findTodo);
+
+  //after deleting todo todos length will be 0
+  expect(screen.queryAllByTestId("todo")).toHaveLength(0);
+});
+
+test("When user try to add emtpy todo it will show error", () => {
+  render(<App />);
+  const addTodoBtn = screen.getByRole("button");
+
+  userEvent.click(addTodoBtn);
+
+  // it shows error because user wants to add emtpy todo
+  expect(
+    screen.getByText("Todo field is empty.Please fill that field")
+  ).toBeInTheDocument();
 });
